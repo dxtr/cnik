f23a0483fe2bdbbff6af4d7ad14bb6887ec926d6
Jester <cosmicruz@(none).(none)>
2012-04-26 01:14:38 -0500

    moar stuff

--- src/client.c
+++ src/client.c
@@ -1,6 +1,6 @@
 #include "client.h"
 
-struct Client *clientcreate(const char *nickname, const  char *username, void *channels, const char *server, short unsigned  int port)
+struct Client *clientCreate(const char *nickname, const  char *username, void *channels, const char *server, short unsigned  int port)
 {
 	struct Client *c = malloc(sizeof(struct Client));
 	
@@ -17,36 +17,45 @@ struct Client *clientcreate(const char *nickname, const  char *username, void *c
 
 void clientDestroy(struct Client *client)
 {
+	/* Todo:  Make some fun stuff here */
 }
 
 void clientConnect(struct Client *client)
 {
+	/* Todo:  Make other fun stuff here */
 }
 
 void clientDisconnect(struct Client *client, const char *message)
 {
+	/* Todo:  Some stuff */
 }
 
 void clientPart(struct Client *client, const char *channel)
 {
+	/* Todo:  I need to learn how to code */
 }
 
 void clientJoin(struct client *client, const char *channel, const char *password)
 {
+	/* Todo:  Is this done yet? */
 }
 
 void clientSetNick(struct Client *client, const char *nickname)
 {
+	strncpy(client->nickname, nickname, 64);
 }
 
 void clientSetServer(struct Client *client, const char *server)
 {
+	strncpy(client->server, server, 64);
 }
 
 void clientSetUsername(struct Client *client, const char *username)
 {
+	strncpy(client->username, username, 64);
 }
 
 void clientSetPort(struct Client *client, short unsigned int port)
 {
+	client->port = port;
 }
