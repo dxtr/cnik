58264ec2029dcffa2140a78dbf03812b9f330146
Jester <cosmicruz@(none).(none)>
2012-04-26 00:32:10 -0500

    declared some functions

--- src/client.h
+++ src/client.h
@@ -3,7 +3,11 @@
 
 struct Client 
 {
-	char[64] nickname, username, void *channels, server;
+	char[64] nickname;
+	char[64] username;
+	char[64] server;
+	void *channels;
+
 	short unsigned int port;
 	void *socket;
 
@@ -11,6 +15,15 @@ struct Client
 
 }
 struct Client *clientCreate(char *nickname, char *username, void *channels, char *server, int port);
+void clientDestroy(struct Client *client);
+void clientConnect(struct Client *client);
+void clientDisconnect(struct Client *client, const char *message);
+void clientPart(struct Client *client, const char *channel);
+void clientJoin(struct Client *client, const char *channel, const char *password);
 
+void clientSetNick(struct Client *client, char *nickname);
+void clientSetServer(struct Client *client, char *server);
+void clientSetUsername(struct Client *client, char *username);
+void clientSetPort(struct Client *client, int port);
 
 #endif
