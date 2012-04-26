7f44dfd8d87424535fdfb1b9e46e3b178cb4b159
Jester <cosmicruz@(none).(none)>
2012-04-26 01:07:51 -0500

    made skeletons of functions

--- src/client.c
+++ src/client.c
@@ -1,6 +1,52 @@
 #include "client.h"
 
-struct Client *clientcreate(char *nickname, char *username, void *channels, char *server, int port)
+struct Client *clientcreate(const char *nickname, const  char *username, void *channels, const char *server, short unsigned  int port)
 {
+	struct Client *c = malloc(sizeof(struct Client));
+	
+	memset(c,0,sizeof(sturct Client));
 
+	if(c)
+	{
+		strncpy(c->nickname, nickname, 64);
+		strncpy(c->username, username, 64);
+		strncpy(c->server, server, 64);
+		c->port = port;
+	}
+}
+
+void clientDestroy(struct Client *client)
+{
+}
+
+void clientConnect(struct Client *client)
+{
+}
+
+void clientDisconnect(struct Client *client, const char *message)
+{
+}
+
+void clientPart(struct Client *client, const char *channel)
+{
+}
+
+void clientJoin(struct client *client, const char *channel, const char *password)
+{
+}
+
+void clientSetNick(struct Client *client, const char *nickname)
+{
+}
+
+void clientSetServer(struct Client *client, const char *server)
+{
+}
+
+void clientSetUsername(struct Client *client, const char *username)
+{
+}
+
+void clientSetPort(struct Client *client, short unsigned int port)
+{
 }
--- src/client.h
+++ src/client.h
@@ -3,27 +3,25 @@
 
 struct Client 
 {
-	char[64] nickname;
-	char[64] username;
-	char[64] server;
+	char[65] nickname;
+	char[65] username;
+	char[65] server;
 	void *channels;
 
 	short unsigned int port;
 	void *socket;
+};
 
-	client();
-
-}
-struct Client *clientCreate(char *nickname, char *username, void *channels, char *server, int port);
+struct Client *clientCreate(const char *nickname, const char *username, void *channels, const char *server, short unsigned int port);
 void clientDestroy(struct Client *client);
 void clientConnect(struct Client *client);
 void clientDisconnect(struct Client *client, const char *message);
 void clientPart(struct Client *client, const char *channel);
 void clientJoin(struct Client *client, const char *channel, const char *password);
 
-void clientSetNick(struct Client *client, char *nickname);
-void clientSetServer(struct Client *client, char *server);
-void clientSetUsername(struct Client *client, char *username);
-void clientSetPort(struct Client *client, int port);
+void clientSetNick(struct Client *client, const char *nickname);
+void clientSetServer(struct Client *client, const char *server);
+void clientSetUsername(struct Client *client, const char *username);
+void clientSetPort(struct Client *client, short unsigned int port);
 
 #endif
